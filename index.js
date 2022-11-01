5abf83eff974a8d0a33ae633982d2abb0473f4cf
shinar3@gmail.com <110408797+shenar-coder@users.noreply.github.com>
2022-11-01 10:14:32 -0400

    commit tree 8e23f30053b9181f08d87c78821b7def73afbf74

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+const num1 = 2;
+
+const num2 = 31;
+
+const num4=5;
+
+let multiply = (num1 * num2);
+
+const random = Math.ceil(Math.random() * 10);
+
+let num3 =24
+
+let mod =num3% num4
+
+const max = Math.max(5, 10, -15, 20 );
+
+
+
+
+
+
+
+
+
+
+
+
+
